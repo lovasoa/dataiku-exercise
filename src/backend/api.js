202463e60d41d871a42df8c80d6f0fc8115dfe49
Ophir LOJKINE <pere.jobs@gmail.com>
2017-01-22 20:25:30 +0300

    Display the number of truncated values
    Former-commit-id: 997a902f8a8dd8a5ed21d0746cfa0611d6ef71d4

--- src/backend/api.js
+++ src/backend/api.js
@@ -19,21 +19,34 @@ app.use('/static', express.static(STATIC_DIR));
 
 // Serve our api on /api
 const data = express().get('/data/:column', function(req, res){
-  const SQL_SELECT = `
-    SELECT
-      AVG(census.age) AS age, COUNT(*) AS samples, census.value
-    FROM census
-    WHERE census.column = ?
-    GROUP BY census.value
-    ORDER BY samples DESC
-    LIMIT 100
+  const SQL_VALUES = `
+    WITH results AS (
+      SELECT
+        AVG(census.age) AS age, COUNT(*) AS samples, census.value AS value
+      FROM census
+      WHERE census.column = ?
+      GROUP BY census.value
+      ORDER BY samples DESC
+    )
+      SELECT s1.* FROM (SELECT * FROM results LIMIT 100) AS s1
+    UNION ALL
+      SELECT * FROM (
+          SELECT
+          AVG(others.age) AS age,
+          SUM(others.samples) AS samples,
+          ('Other (' || COUNT(others.value) || 'distinct values)') AS value
+          FROM (SELECT * FROM results LIMIT 100000 OFFSET 100) AS others
+          WHERE others.value IS NOT NULL
+      )
+      WHERE samples IS NOT NULL
   `;
-  sqlite.all(SQL_SELECT, [req.params.column]).then((data) =>
+  const SQL_NAME = `SELECT name FROM COLUMNS WHERE id = ?`;
+  sqlite.all(SQL_VALUES, [req.params.column]).then((data) =>
     res.json({data, column: req.params.column})
   ).catch((err) => res.status(500).json({'error': err}));
 });
 const columns = express().get('/columns', function(req, res){
-  const SQL_SELECT = `SELECT * FROM columns`;
+  const SQL_SELECT = `SELECT id, name FROM columns`;
   sqlite.all(SQL_SELECT, [req.params.column]).then((data) =>
     res.json({data})
   ).catch((err) => res.status(500).json({'error': err}));
